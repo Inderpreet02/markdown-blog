4c91f39db2b8ebbef073ed877d24843dae4e13e6
Inderpreet02 <inder192002@gmail.com>
2022-08-21 23:59:36 +0530

    mongodb to be added

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+// mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine', 'ejs')
 
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const article = [
+        {
+        title: 'Test Article',
+        createdAt: new Date(),
+        desc: 'This is a test description'
+        },
+        {
+        title: 'Test Article',
+        createdAt: new Date(),
+        desc: 'This is a test description'
+        },
+    ]
+    res.render('articles/index', {articles : article})
 })
 
-app.listen(5000, (req, res) => {
+app.listen(5000, () => {
     console.log("Server is up");
 }); 
